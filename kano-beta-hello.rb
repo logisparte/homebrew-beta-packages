3da4588ffa5846fbb814f1bf46e2e9a349e7e09f
Philippe Desjardins <pdesjardins90@users.noreply.github.com>
2022-06-22 14:44:02 -0400

    x(kano): released beta-hello

---
+++ kano-beta-hello.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+class KanoBetaHello < Formula
+  desc "Software engineering task management CLI"
+  homepage "https://github.com/logisparte/kano"
+  url "https://github.com/logisparte/kano/releases/download/beta-hello/kano.tar.gz"
+  version "beta-hello"
+  sha256 "d251ba8ce9f4015711f9587bfb7d3d93af3b499cc8bbb4fc30d8fe97abdeec3d"
+  license "GPL-3.0-only"
+  depends_on "docker" => :optional
+  depends_on "git" => :optional
+
+  def install
+    libexec.install Dir["libexec/*"]
+    doc.install Dir["share/doc/*"]
+    prefix.install "LICENSE"
+    bin.write_exec_script (libexec/"kano")
+  end
+
+  test do
+    help_output = shell_output("kano help")
+    assert_match(/Show this help message/, help_output)
+  end
+end
