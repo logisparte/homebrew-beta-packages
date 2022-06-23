16d4288c593d29c75d14e0a14a13dcecbc9f1b8d
helot <logisparte-helot@users.noreply.github.com>
2022-06-23 16:12:28 +0000

    x(kano-beta-hello): released a167e9a

--- kano-beta-hello.rb
+++ kano-beta-hello.rb
@@ -4,8 +4,8 @@ class KanoBetaHello < Formula
   desc "Software engineering task management CLI"
   homepage "https://github.com/logisparte/kano"
   url "https://github.com/logisparte/kano/releases/download/beta-hello/kano.tar.gz"
-  version "0.0.0-0e5e540"
-  sha256 "9cc6fc7a324b9d9ea217785e0b26e978bd287c64600580ff19904542a8debb6e"
+  version "0.0.0-a167e9a"
+  sha256 "77b92c919e631793bcb139cf515e440ccca14a059b0b88d7723f722e7e998eae"
   license "GPL-3.0-only"
   depends_on "docker" => :optional
   depends_on "git" => :optional
