b3132372c6887194662d576e9fd1e9b8d3691ef8
helot <logisparte-helot@users.noreply.github.com>
2022-07-01 02:50:14 +0000

    x(kano-beta-docker-v4): released 4324d59

---
+++ kano-beta-docker-v4.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+class KanoBetaDockerV4 < Formula
+  desc "Software engineering workflow automation CLI"
+  homepage "https://github.com/logisparte/kano"
+  url "https://github.com/logisparte/kano/releases/download/beta-docker-v4/kano.tar.gz"
+  version "0.0.0-4324d59"
+  sha256 "ba26b0ff22701435f7761e3dfbb3e2d7bf2db7a6376fe3d450feddaeab6c1492"
+  license "GPL-3.0-only"
+  depends_on "git" => :optional
+
+  def install
+    libexec.install Dir["libexec/*"]
+    doc.install Dir["share/doc/*"]
+    prefix.install "LICENSE"
+    bin.write_exec_script (libexec/"kano")
+  end
+
+  def caveats
+    <<~EOF
+      Homebrew formula does not support this syntax:
+        depends_on cask: "docker" => :optional
+
+      So to (optionally) install Docker Desktop, do:
+        brew install --cask docker
+    EOF
+  end
+
+  test do
+    help_output = shell_output("kano help")
+    assert_match(/Show this help message/, help_output)
+  end
+end
