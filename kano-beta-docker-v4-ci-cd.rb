aaf2eb0cb6c54519598f20207f1f83bb01c26afd
helot <logisparte-helot@users.noreply.github.com>
2022-07-04 22:41:50 +0000

    x(kano-beta-docker-v4-ci-cd): released 968441a

--- kano-beta-docker-v4-ci-cd.rb
+++ kano-beta-docker-v4-ci-cd.rb
@@ -4,8 +4,8 @@ class KanoBetaDockerV4CiCd < Formula
   desc "Software engineering workflow automation CLI"
   homepage "https://github.com/logisparte/kano"
   url "https://github.com/logisparte/kano/releases/download/beta-docker-v4-ci-cd/kano.tar.gz"
-  version "0.0.0-ca00ca2"
-  sha256 "02c5be2b8bb143bafdd5de6b058c9c4993f8d1f94fcddcc861c44a921d9596e1"
+  version "0.0.0-968441a"
+  sha256 "b68c37caf482b09c68597dc01e1f0d2d4a0755bfc066a0d73ae61a4d27b7ee12"
   license "GPL-3.0-only"
   depends_on "git" => :optional
 
